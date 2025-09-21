94125ee3b4055a6409b90bab74e555b3337d56a3
Luka <luka.djordjevic@elfak.rs>
2025-09-21 21:47:28 +0200

    Dodata funkcionalnost dodavanja markera

--- app/src/main/java/com/example/trapzoneapp/screens/main/MapScreen.kt
+++ app/src/main/java/com/example/trapzoneapp/screens/main/MapScreen.kt
@@ -2,20 +2,28 @@ package com.example.trapzoneapp.screens.main
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
@@ -69,6 +77,7 @@ fun MapScreen(modifier: Modifier=Modifier)
             }
         }
     }
+    val markers = remember { mutableStateListOf<LatLng>() }
     if (locationPermission.status.isGranted) {
 
         GoogleMap(
@@ -82,6 +91,28 @@ fun MapScreen(modifier: Modifier=Modifier)
                 title = "Vaša lokacija",
                 icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
             )
+            markers.forEach { latLng ->
+                Marker(
+                    state = MarkerState(position = latLng),
+                    title = "Objekat",
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+                )
+
+            }
+        }
+        FloatingActionButton(
+            onClick = {
+                userLocation.let {
+                    markers.add(it)
+                    Toast.makeText(context, "Marker je dodat na vašoj trenutnoj lokaciji!", Toast.LENGTH_SHORT).show()
+
+                }
+            },
+            modifier = Modifier
+                //.align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "Dodaj objekat")
         }
     } else {
         Column(
